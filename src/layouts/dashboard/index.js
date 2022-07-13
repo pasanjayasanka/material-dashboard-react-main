03511134c48da029c2215cf57399ee8a21de6a9b
Pasan jayasanka <pasanjayasanka7@gmail.com>
2022-07-14 00:00:28 +0530

    first commit

---
+++ src/layouts/dashboard/index.js
@@ -0,0 +1,167 @@
+/**
+=========================================================
+* Material Dashboard 2 React - v2.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Grid from "@mui/material/Grid";
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+
+// Material Dashboard 2 React example components
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import Footer from "examples/Footer";
+import ReportsBarChart from "examples/Charts/BarCharts/ReportsBarChart";
+import ReportsLineChart from "examples/Charts/LineCharts/ReportsLineChart";
+import ReportsPieChart from "examples/Charts/PieChart";
+import ComplexStatisticsCard from "examples/Cards/StatisticsCards/ComplexStatisticsCard";
+
+// Data
+import reportsBarChartData from "layouts/dashboard/data/reportsBarChartData";
+import reportsLineChartData from "layouts/dashboard/data/reportsLineChartData";
+
+// Dashboard components
+// import Projects from "layouts/dashboard/components/Projects";
+// import OrdersOverview from "layouts/dashboard/components/OrdersOverview";
+
+function Dashboard() {
+  const { sales, tasks } = reportsLineChartData;
+
+  return (
+    <DashboardLayout>
+      <DashboardNavbar />
+      <MDBox py={3}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={6} lg={3}>
+            <MDBox mb={1.5}>
+              <ComplexStatisticsCard
+                color="primary"
+                icon="timer"
+                title="Training Time"
+                count="3h 25m"
+                percentage={{
+                  color: "success",
+                  amount: "+55%",
+                  label: "than lask week",
+                }}
+              />
+            </MDBox>
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <MDBox mb={1.5}>
+              <ComplexStatisticsCard
+                icon="leaderboard"
+                title="ACWR"
+                count="2,300"
+                percentage={{
+                  color: "success",
+                  amount: "+3%",
+                  label: "than last month",
+                }}
+              />
+            </MDBox>
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <MDBox mb={1.5}>
+              <ComplexStatisticsCard
+                color="success"
+                icon="favorite"
+                title="Average Heart Rate"
+                count="95 Pulse"
+                percentage={{
+                  color: "success",
+                  amount: "+1%",
+                  label: "than yesterday",
+                }}
+              />
+            </MDBox>
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <MDBox mb={1.5}>
+              <ComplexStatisticsCard
+                color="primary"
+                icon="local_hospital"
+                title="TRIMP Score"
+                count="1.5"
+                percentage={{
+                  color: "success",
+                  amount: "+1%",
+                  label: "than yesterday",
+                }}
+              />
+            </MDBox>
+          </Grid>
+        </Grid>
+        <MDBox mt={4.5}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={6} lg={4}>
+              <MDBox mb={3}>
+                <ReportsBarChart
+                  color="info"
+                  title="ACWR value"
+                  description="Change During the week"
+                  chart={reportsBarChartData}
+                />
+              </MDBox>
+            </Grid>
+            <Grid item xs={12} md={6} lg={4}>
+              <MDBox mb={3}>
+                <ReportsLineChart
+                  color="success"
+                  title="Average Heart Rate"
+                  description="Change During the week"
+                  chart={sales}
+                />
+              </MDBox>
+            </Grid>
+            <Grid item xs={12} md={6} lg={4}>
+              <MDBox mb={3}>
+                <ReportsLineChart
+                  color="primary"
+                  title="TRIMP Score"
+                  description="Change During the week"
+                  chart={tasks}
+                />
+              </MDBox>
+            </Grid>
+            <Grid item xs={12} md={6} lg={4}>
+              <MDBox mb={3}>
+                <ReportsPieChart
+                  color="info"
+                  title="Activity Time"
+                  description="Change During the week"
+                  chart={sales}
+                />
+              </MDBox>
+            </Grid>
+          </Grid>
+        </MDBox>
+        {/* <MDBox>
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={6} lg={8}>
+              <Projects />
+            </Grid>
+            <Grid item xs={12} md={6} lg={4}>
+              <OrdersOverview />
+            </Grid>
+          </Grid>
+        </MDBox> */}
+      </MDBox>
+      <Footer />
+    </DashboardLayout>
+  );
+}
+
+export default Dashboard;
---
+++ src/layouts/tables/data/projectsTableData.js
@@ -0,0 +1,122 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/function-component-definition */
+/**
+=========================================================
+* Material Dashboard 2 React - v2.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+// import Icon from "@mui/material/Icon";
+
+// Material Dashboard 2 React components
+// import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+// import MDAvatar from "components/MDAvatar";
+// import MDProgress from "components/MDProgress";
+
+// Images
+// import LogoAsana from "assets/images/small-logos/logo-asana.svg";
+// import logoGithub from "assets/images/small-logos/github.svg";
+// import logoAtlassian from "assets/images/small-logos/logo-atlassian.svg";
+// import logoSlack from "assets/images/small-logos/logo-slack.svg";
+// import logoSpotify from "assets/images/small-logos/logo-spotify.svg";
+// import logoInvesion from "assets/images/small-logos/logo-invision.svg";
+
+export default function data() {
+  // const Project = ({ image, name }) => (
+  //   <MDBox display="flex" alignItems="center" lineHeight={1}>
+  //     <MDAvatar src={image} name={name} size="sm" variant="rounded" />
+  //     <MDTypography display="block" variant="button" fontWeight="medium" ml={1} lineHeight={1}>
+  //       {name}
+  //     </MDTypography>
+  //   </MDBox>
+  // );
+
+  // const Progress = ({ color, value }) => (
+  //   <MDBox display="flex" alignItems="center">
+  //     <MDTypography variant="caption" color="text" fontWeight="medium">
+  //       {value}%
+  //     </MDTypography>
+  //     <MDBox ml={0.5} width="9rem">
+  //       <MDProgress variant="gradient" color={color} value={value} />
+  //     </MDBox>
+  //   </MDBox>
+  // );
+
+  return {
+    columns: [
+      { Header: "Session Date", accessor: "project", width: "30%", align: "left" },
+      { Header: "Session Time", accessor: "budget", align: "left" },
+      { Header: "ACWR", accessor: "status", align: "center" },
+      { Header: "Avg Heart Rate", accessor: "completion", align: "center" },
+      { Header: "TRIMP", accessor: "action", align: "center" },
+    ],
+
+    rows: [
+      {
+        project: (
+          <MDTypography component="a" href="#" variant="button" color="text" fontWeight="medium">
+            10/07/2022
+          </MDTypography>
+        ),
+        budget: (
+          <MDTypography component="a" href="#" variant="button" color="text" fontWeight="medium">
+            3h 25min
+          </MDTypography>
+        ),
+        status: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            2.4
+          </MDTypography>
+        ),
+        completion: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            95 Pulse
+          </MDTypography>
+        ),
+        action: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            1.5
+          </MDTypography>
+        ),
+      },
+      {
+        project: (
+          <MDTypography component="a" href="#" variant="button" color="text" fontWeight="medium">
+            12/07/2022
+          </MDTypography>
+        ),
+        budget: (
+          <MDTypography component="a" href="#" variant="button" color="text" fontWeight="medium">
+            2h 15min
+          </MDTypography>
+        ),
+        status: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            1.8
+          </MDTypography>
+        ),
+        completion: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            113 Pulse
+          </MDTypography>
+        ),
+        action: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            0.8
+          </MDTypography>
+        ),
+      },
+    ],
+  };
+}
